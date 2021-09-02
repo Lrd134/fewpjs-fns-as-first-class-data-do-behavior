c5b3ef4fbc3422503ea2188c35dcedaf1cb903b2
Lrd134 <larryc3200@gmail.com>
2021-09-02 10:13:36 -0400

    commit tree 8fc7996acf63b406f8c04953ad0c633b70b6a216

--- index.js
+++ index.js
@@ -10,4 +10,17 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time) {
+  time = time.split(':');
+  if (parseInt(10, time[0]) > 17) {
+    return "Good Evening";
+  } else if ((parseInt(10, time[0]) > 12) && (parseInt(10, time[0]) < 17)) {
+  return "Good Afternoon";
+  } else if (parseInt(10, time[0]) < 12) {
+    return "Good Morning";
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(string) {
+  document.getElementById("greeting").innerHTML = string;
+}
